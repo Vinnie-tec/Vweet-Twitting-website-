a8c70674a22dd29f93e70944a0c0e8c84da29b33
Agboola Vincent <agboolavin65@gmail.com>
2023-04-24 12:44:26 +0100

    Added Modals for login and register

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React, { useCallback } from "react";
+// import { useRouter } from "next/router";
 import { FaFeather } from "react-icons/fa";
 
+import useLoginModal from "@/hooks/useLoginModal";
+
 const SidebarTweetButton = () => {
-  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const onClick = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
   return (
-    <div onClick={() => router.push("/")}>
+    <div onClick={onClick}>
       <div
         className="
     mt-6
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,18 @@ import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
+import RegisterModal from "@/components/modals/RegisterModals";
+// import Modal from "@/components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
