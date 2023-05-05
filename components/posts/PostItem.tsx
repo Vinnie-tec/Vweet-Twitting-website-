891256713ce9692f62ccba8bc846575726b8a1ac
Agboola Vincent <agboolavin65@gmail.com>
2023-05-05 18:22:51 +0100

    Added comment and like feature

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -5,7 +5,7 @@ import { formatDistanceToNowStrict } from "date-fns";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useCurrentUser from "@/hooks/useCurrentUser";
-// import useLike from "@/hooks/useLike";
+import useLike from "@/hooks/useLike";
 
 import Avatar from "../Avatar";
 interface PostItemProps {
@@ -18,7 +18,7 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
   const loginModal = useLoginModal();
 
   const { data: currentUser } = useCurrentUser();
-//   const { hasLiked, toggleLike } = useLike({ postId: data.id, userId });
+  const { hasLiked, toggleLike } = useLike({ postId: data.id, userId });
 
   const goToUser = useCallback(
     (ev: any) => {
@@ -40,12 +40,12 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
         return loginModal.onOpen();
       }
 
-    //   toggleLike();
+      toggleLike();
     },
-    [loginModal, currentUser, ]
+    [loginModal, currentUser, toggleLike ]
   );
 
-//   const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
+  const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
 
   const createdAt = useMemo(() => {
     if (!data?.createdAt) {
@@ -126,7 +126,7 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 hover:text-red-500
             "
             >
-              {/* <LikeIcon color={hasLiked ? "red" : ""} size={20} /> */}
+              <LikeIcon color={hasLiked ? "red" : ""} size={20} />
               <p>{data.likedIds.length}</p>
             </div>
           </div>
--- hooks/usePost.ts
+++ hooks/usePost.ts
@@ -1,16 +1,19 @@
-import useSWR from 'swr';
+import useSWR from "swr";
 
-import fetcher from '@/libs/fetcher';
+import fetcher from "@/libs/fetcher";
 
 const usePost = (postId: string) => {
-  const { data, error, isLoading, mutate } = useSWR(postId ? `/api/posts/${postId}` : null, fetcher);
+  const { data, error, isLoading, mutate } = useSWR(
+    postId ? `/api/posts/${postId}` : null,
+    fetcher
+  );
 
   return {
     data,
     error,
     isLoading,
-    mutate
-  }
+    mutate,
+  };
 };
 
 export default usePost;
